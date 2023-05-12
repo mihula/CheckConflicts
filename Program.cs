285c77b38caa3ba08ed29d97c441d2ab1042f923
René Mihula <mihula@provys.com>
2023-05-13 00:21:43 +0200

    vylepseni - konflikt je to jen kdyz radky z headu nenajdu v tom merge

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Text;
 
 namespace CheckConflicts
@@ -17,64 +19,150 @@ namespace CheckConflicts
             else
             {
                 // debug
-                path = @"c:\Provys\amcni_tst\_net\src\";
+                path = @"c:\Provys\mtva_proj\_net\src\";
             }
 
             var files = Directory.GetFiles(path, "*.cs", SearchOption.AllDirectories);
             foreach (var file in files)
             {
                 //Console.WriteLine($"File: {file}");
-                var isInConflict = false;
-                var linesOfConflict = 0;
-                var conflictText = new StringBuilder();
+                var inConflictState = ConflictState.None;
+                var conflictSet = new ConflictSet();
+
                 using (var fs = File.OpenRead(file))
                 using (var sr = new StreamReader(fs))
                 {
                     string line = string.Empty;
                     while ((line = sr.ReadLine()) != null)
                     {
-                        if (line.StartsWith("namespace"))
+                        if (string.IsNullOrWhiteSpace(line)) continue;
+
+                        if (line.StartsWith("<<<<<<<"))
                         {
-                            //Console.WriteLine("...namespace > end of looking");
-                            break;
+                            inConflictState = ConflictState.Head;
+                            conflictSet.StartConflict();
+                            continue;
                         }
                         if (line.StartsWith("======="))
                         {
-                            if (linesOfConflict == 0)
-                            {
-                                conflictText.Clear();
-                                break;
-                            }
-                        }
-                        if (isInConflict)
-                        {
-                            linesOfConflict++;
-                        }
-                        if (line.StartsWith("<<<<<<< HEAD"))
-                        {
-                            //Console.WriteLine("...found conflict tag");
-                            isInConflict = true;
-                        }
-                        if (isInConflict)
-                        {
-                            conflictText.AppendLine(line);
+                            inConflictState = ConflictState.Merge;
+                            continue;
                         }
                         if (line.StartsWith(">>>>>>>"))
                         {
-                            //Console.WriteLine("...found end-conflict tag");
-                            isInConflict = false;
+                            inConflictState = ConflictState.None;
+                            conflictSet.EndConflict();
+                            continue;
+                        }
+                        if (line.StartsWith("namespace"))
+                        {
+                            break;
+                        }
+
+                        if (conflictSet.ActiveConflict == null) continue;
+
+                        if (line.ToLowerInvariant().Contains("// modification history")) continue; // zbytecne moc konfliktu, nepotrebujem toto
+                        if (line.StartsWith("// -------------------------------------")) continue; // zbytecne moc konfliktu, nepotrebujem toto
+
+                        line = line.Replace("prazak", "doubek").Trim(); // nazdar zdendo ty lumpe!
+
+                        switch (inConflictState)
+                        {
+                            case ConflictState.None:
+                                // nemelo by se to sem dostat - nemel by byt zadny conflict Active
+                                continue;
+
+                            case ConflictState.Head:
+                                conflictSet.ActiveConflict.HeadLines.Add(line);
+                                break;
+
+                            case ConflictState.Merge:
+                                conflictSet.ActiveConflict.MergeLines.Add(line);
+                                break;
                         }
                     }
                 }
-                if (conflictText.Length > 0)
+
+                if (conflictSet.Evaluate())
                 {
-                    Console.WriteLine($"File has real conflict: {file}");
-                    Console.WriteLine(conflictText.ToString());
+                    Console.WriteLine($"{file} <<< file has real conflict");
+                    Console.WriteLine(conflictSet.ToString());
                     Console.WriteLine(new string('-', 80));
                 }
             }
 
             //Console.ReadKey();
+        }
+    }
+
+    enum ConflictState
+    {
+        None,
+        Head,
+        Merge
+    }
+
+    class ConflictSet
+    {
+        public List<Conflict> Conflicts { get; private set; } = new List<Conflict>();
+        public Conflict ActiveConflict { get; private set; } = null;
+
+        internal void StartConflict()
+        {
+            Conflict result = new Conflict();
+            ActiveConflict = result;
+            Conflicts.Add(result);
+        }
+
+        internal void EndConflict()
+        {
+            ActiveConflict = null;
+        }
+
+        internal bool Evaluate()
+        {
+            var result = false;
+            foreach (var conflict in Conflicts)
+            {
+                result = conflict.Evaluate() | result;
+            }
+            return result;
+        }
+
+        public override string ToString()
+        {
+            var result = new StringBuilder();
+            foreach (var conflict in Conflicts)
+            {
+                if (conflict.HeadLines.Count > 0)
+                    result.Append(conflict.ToString());
+            }
+            return result.ToString();
+        }
+    }
+
+    public class Conflict
+    {
+        public List<string> HeadLines { get; private set; } = new List<string>();
+        public List<string> MergeLines { get; private set; } = new List<string>();
+        internal bool Evaluate()
+        {
+            // vsechny HeadLines co jsou v MergeLines vyhodim a pokud neco v head zustalo > true jinak >false (neni to conflict)
+            var newHeadLines = HeadLines.Where(line => !MergeLines.Contains(line)).ToList();
+            var newMergeLines = MergeLines.Where(line => !HeadLines.Contains(line)).ToList();
+            HeadLines = newHeadLines;
+            MergeLines = newMergeLines;
+            return HeadLines.Count > 0;
+        }
+        public override string ToString()
+        {
+            var result = new StringBuilder();
+            result.AppendLine("<<<<<<< HEAD");
+            foreach(var line in HeadLines) result.AppendLine(line);
+            result.AppendLine("============");
+            foreach (var line in MergeLines) result.AppendLine(line);
+            result.AppendLine("MERGE >>>>>>");
+            return result.ToString();
         }
     }
 }
